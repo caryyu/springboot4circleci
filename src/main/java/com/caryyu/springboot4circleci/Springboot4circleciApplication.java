bd065473dcd6da89cb90a07c226ca82be45624e7
caryyu <343194291@qq.com>
2018-08-30 18:11:03 +0800

    init commit

---
+++ src/main/java/com/caryyu/springboot4circleci/Springboot4circleciApplication.java
@@ -0,0 +1,12 @@
+package com.caryyu.springboot4circleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4circleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot4circleciApplication.class, args);
+	}
+}
---
+++ src/test/java/com/caryyu/springboot4circleci/Springboot4circleciApplicationTests.java
@@ -0,0 +1,16 @@
+package com.caryyu.springboot4circleci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot4circleciApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
